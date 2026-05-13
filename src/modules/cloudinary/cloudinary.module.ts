134d8bb484e59bdae54616abd15694da87dc8da0
Mohamed-Mansour11 <153311578+Mohamed-Mansour11@users.noreply.github.com>
2026-05-14 02:00:58 +0300

    Edit

--- src/modules/cloudinary/cloudinary.module.ts
+++ src/modules/cloudinary/cloudinary.module.ts
@@ -1,11 +1,8 @@
 import { Module } from '@nestjs/common';
-import { CloudinaryProvider } from './cloudinary.provider';
 import { CloudinaryService } from './cloudinary.service';
-import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [ConfigModule], // لضمان قراءة مفاتيح Cloudinary من ملف .env
-  providers: [CloudinaryProvider, CloudinaryService],
-  exports: [CloudinaryProvider, CloudinaryService], //  التصدير ضروري لتستخدمه الموديولات الأخرى
+  providers: [CloudinaryService],
+  exports: [CloudinaryService],
 })
 export class CloudinaryModule {}
--- src/modules/cloudinary/cloudinary.service.ts
+++ src/modules/cloudinary/cloudinary.service.ts
@@ -4,10 +4,21 @@ import {
   UploadApiResponse,
   UploadApiErrorResponse,
 } from 'cloudinary';
+import { ConfigService } from '@nestjs/config'; // استيراد الـ ConfigService
 import * as Multer from 'multer';
 const toStream = require('buffer-to-stream');
+
 @Injectable()
 export class CloudinaryService {
+  //  تهيئة المفاتيح هنا مباشرة لضمان قراءتها 100%
+  constructor(private configService: ConfigService) {
+    cloudinary.config({
+      cloud_name: this.configService.get<string>('CLOUD_NAME'),
+      api_key: this.configService.get<string>('CLOUD_API_KEY'),
+      api_secret: this.configService.get<string>('CLOUD_API_SECRET'),
+    });
+  }
+
   async uploadFile(
     file: Express.Multer.File,
     folder: string = 'crm_files',
